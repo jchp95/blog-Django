aa495250b157c6f415ee8f3a7210ba6beac8ff28
Julio Cesar Hernández Pedraza <juliohp1995@gmail.com>
2024-11-30 22:48:39 +0100

    update

--- mi_app/static/js/slide.js
+++ mi_app/static/js/slide.js
@@ -1,31 +1,30 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let currentIndex = 0;
-    const slides = document.querySelectorAll('.card-slide');
-    const totalSlides = slides.length;
-    const slidesToShow = 4; // Muestra 4 tarjetas a la vez
-    const slidesToMove = 4; // Mueve 4 tarjetas a la vez
+new Swiper('.card-wrapper', {
+  loop: true,
+  spaceBetween: 30,
 
-    function showSlide(index) {
-        const offset = index * -(100 / (totalSlides / slidesToShow)); // Calcula la traslación en función de cuántas tarjetas muestras
-        document.querySelector('.slide-container').style.transform = `translateX(${offset}%)`;
-    }
+  // Pagination bullets
+  pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true
+  },
 
-    // Cambia el tiempo de deslizamiento a 10 segundos (10000 milisegundos)
-    setInterval(() => {
-        currentIndex = (currentIndex + slidesToMove) % Math.ceil(totalSlides / slidesToMove); // Asegúrate de no superar el número de "sets" de tarjetas
-        showSlide(currentIndex);
-    }, 10000); // Cambia de slide cada 10 segundos
+  // Navigation arrows
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+  },
 
-    // Funciones para los botones de navegación
-    document.querySelector('.next').addEventListener('click', () => {
-        currentIndex = (currentIndex + slidesToMove) % Math.ceil(totalSlides / slidesToMove); // Avanza al siguiente set
-        showSlide(currentIndex);
-    });
-
-    document.querySelector('.prev').addEventListener('click', () => {
-        currentIndex = (currentIndex - slidesToMove + Math.ceil(totalSlides / slidesToMove)) % Math.ceil(totalSlides / slidesToMove); // Retrocede al set anterior
-        showSlide(currentIndex);
-    });
+  // Responsive breakpoints
+  breakpoints: {
+      0: {
+          slidesPerView: 1
+      },
+      768: {
+          slidesPerView: 2
+      },
+      1024: {
+          slidesPerView: 3
+      }
+  }
 });
-
-
